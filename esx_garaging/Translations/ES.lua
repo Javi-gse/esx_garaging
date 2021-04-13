bd7ce89cb890e5c235e6bc549263a3dd7ee9650f
Javi-gse <72070422+Javi-gse@users.noreply.github.com>
2021-04-13 23:22:05 +0200

    Create ES.lua
    Spanish translation

---
+++ esx_garaging/Translations/ES.lua
@@ -0,0 +1,42 @@
+--[[
+  _____   _                                 _   _   _
+ |_   _| (_)  _ __    _   _   ___          | \ | | | |
+   | |   | | | '_ \  | | | | / __|         |  \| | | |    
+   | |   | | | | | | | |_| | \__ \         | |\  | | |___ 
+   |_|   |_| |_| |_|  \__,_| |___/  _____  |_| \_| |_____|
+                                   |_____|
+]]--
+
+Translations['ES'] = {
+    ["BUY_GARAGE"] = "Presiona ~INPUT_CONTEXT~ para comprar el garage!",
+    ["MENU_BUY"] = "Comprando el Garage",
+    ["MONEY_BUY"] = "No puedes comprar el garage, no tienes suficiente ~r~dinero~w~ ! Te faltan: ~r~€",
+    ["SUCCES_BUY"] = "El garage ha sido ~g~correctamente~w~ comprado!",
+
+    ["ENTER_GARAGE"] = "Presiona ~INPUT_CONTEXT~ para entrar al garage!",
+    ["LOADING_ENTER_GARAGE"] = "Entering garage...",
+    ["LEAVE_GARAGE"] = "Presiona ~INPUT_CONTEXT~ para salir del garage!",
+    ["LOADING_LEAVE_GARAGE"] = "Saliendo del garage...",
+
+    ["YES"] = "Si",
+    ["NO"] = "No",
+
+    ["ERROR_SPAWN"] = "~r~Algo salió mal al generar el vehículo!",
+    ["BLOCK_SPAWN"] = "~r~¡La puerta del garaje está bloqueada! Asegúrese de que haya suficiente espacio para generar su vehículo.",
+
+    ["NO_SPACE"] = "No hay espacio en este garaje.!",
+
+    ["NOT_OWNED"] = "Este vehiculo no es tuyo!",
+
+    ["OPEN_LAPTOP"] = "Presiona ~INPUT_CONTEXT~ para abrir el garage!",
+    ["NAME_LAPTOP"] = 'NO ENCONTRADO',
+    ["MENU_LAPTOP"] = "Garage portatil",
+    ["IMPOUND_LAPTOP"] = "Confiscado",
+    ["SELL_LAPTOP"] = "Vender garage",
+    ["SUCCES_LAPTOP"] = "El garage ha sido ~g~correctamente~w~ vendido!",
+    ["ERROR_LAPTOP"] = "No puedes vender tu garage inicial!",
+    ["EMPTY_LAPTOP"] = "Necesitas que el garage este vacio!",
+    ["MONEY_LAPTOP"] = "No dispones de suficiente ~r~dinero~w~! Necesitas: ~r~€",
+    ["SUCCES_LAPTOP"] = "El vehículo ha sido ~g~exitosamente~w~ devuelto a su garaje.!",
+}
+
